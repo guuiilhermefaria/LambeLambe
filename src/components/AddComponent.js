6935e4ac6e0136062710884b014ecfa3ca9027d7
Guilherme Faria <guuiilhermefaria@gmail.com>
2020-03-02 11:08:27 -0300

    Same situation as before, just fixed the import of the react-redux and that's it.

--- src/components/AddComponent.js
+++ src/components/AddComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { connect } from 'react-native'
+import { connect } from 'react-redux'
 import { addComment } from '../store/actions/posts'
 import {
     View,
@@ -13,7 +13,7 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 class AddComment extends Component {
-    state = {
+    state = { 
         comment: '',
         editMode: false
     }
@@ -23,7 +23,7 @@ class AddComment extends Component {
             postId: this.props.postId,
             comment: {
                 nickname: this.props.name,
-                comment: this.state.comment,
+                comment: this.state.comment
             }
         })
 
@@ -35,14 +35,11 @@ class AddComment extends Component {
         if (this.state.editMode) {
             commentArea = (
                 <View style={styles.container}>
-                    <TextInput
-                        placeholder='Pode comentar...'
-                        style={styles.input}
-                        autoFocus={true}
+                    <TextInput placeholder='Pode comentar...'
+                        style={styles.input} autoFocus={true}
                         value={this.state.comment}
                         onChangeText={comment => this.setState({ comment })}
-                        onSubmitEditing={this.handleAddComment}
-                    />
+                        onSubmitEditing={this.handleAddComment} />
                     <TWF onPress={() => this.setState({ editMode: false })}>
                         <Icon name='times' size={15} color='#555' />
                     </TWF>
@@ -52,9 +49,9 @@ class AddComment extends Component {
             commentArea = (
                 <TWF onPress={() => this.setState({ editMode: true })}>
                     <View style={styles.container}>
-                        <Icon name='comment-o' size={15} color='#555' />
+                        <Icon name='comment-o' size={25} color='#555' />
                         <Text style={styles.caption}>
-                            Adicione um comentário
+                            Adicione um comentário...
                         </Text>
                     </View>
                 </TWF>
@@ -74,15 +71,15 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        margin: 10,
+        margin: 10
     },
     caption: {
         marginLeft: 10,
         fontSize: 12,
-        color: '#ccc'
+        color: '#CCC'
     },
     input: {
-        width: '90%',
+        width: '90%'
     }
 })
 
